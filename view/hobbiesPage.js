68cf14b55e5c7e71a63e56e5aa8472d6d1640569
karina <crisan.karina31@yahoo.com>
2018-09-20 19:44:35 +0300

    changes

--- view/hobbiesPage.js
+++ view/hobbiesPage.js
@@ -5,29 +5,64 @@ console.log("hello");
     // Reading
     var quoteContainer = $("#quoteContainerId");
 
-    var getQuote = function(myValue) {
-        $.ajax({
-            url: "https://got-quotes.herokuapp.com/quotes?char=" + myValue,
-            success: displayQuote
-        });
+    function displayQuote (response) {
+        var quote = response.character + ": " + response.quote;
+        console.log(response);
+        quoteContainer.html(quote);
     }
 
-    var allRadioButtons = $('input[name="character"]');
-    for(var i = 0; i < allRadioButtons.length; i++){
-        allRadioButtons[i].addEventListener("click", getValue);
-    }
-
-    var getValue = function(myValue){
+    function getValue(myValue){
         var myValue = $('input[name="character"]:checked').val();
         console.log(myValue);
         getQuote(myValue)
     }
 
-    var displayQuote = function(response) {
-        var quote = response.character + ": " + response.quote;
-        console.log(response);
-        quoteContainer.html(quote);
+    var getQuote = function(myValue) {
+        $.ajax({
+            url: "https://got-quotes.herokuapp.com/quotes?char=" + myValue,
+            success: displayQuote
+        });
+        console.log(myValue);
+
     }
+
+    var allRadioButtons = $('input[name="character"]');
+    for(var i = 0; i < allRadioButtons.length; i++){
+        allRadioButtons[i].addEventListener("click", getValue);
+        console.log(getValue);
+    }
+
+
+    //TvSeries
+
+    var getTvSeries = function(show) {
+        $.ajax({
+            url: "http://api.tvmaze.com/search/shows?q=" + show,
+            success: displayTvSeries(show)
+        })
+    }
+
+    function displayTvSeries(response) {
+        var showList = '<ul>'
+        for(i = 0; i < response.length; i++) {
+            var showName = response[i].show.name;
+            var url = response[i].show.url;
+            showList += `<li>` + showName + ` ` + `<a href="` + url + `" target = "_blank"> ` + url + `</a>` + `</li>`
+
+        }
+        showList += '</ul>'
+        return showList;
+        var tvSeriesContainer = $("#tvSeriesContainerId");
+
+        tvSeriesContainer.html(showList);
+    }
+
+    $('button[name="tvSeriesButton"]').on("click", function(){
+        var shows = $("#seriesId").val();
+        getTvSeries(shows);
+    })
+
+
 
     //Joke
     var jokeContainer = $("#jokeContainerId");
@@ -36,7 +71,7 @@ console.log("hello");
         $.ajax({
             url: "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke",
             success: displayJoke
-        })
+       })
     }
 
     function displayJoke(response) {
@@ -50,6 +85,7 @@ console.log("hello");
     }
 
     $('button[name="jokeButton"]').on("click", randomJoke);
+
 
 
 
